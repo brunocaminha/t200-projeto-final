5582ff4c4f175ee33ebdfc91eb7275838bb7a116
Bruno Caminha <brunocaminha4@gmail.com>
2026-06-15 09:31:40 -0300

    Setup Spring Boot 4 + estrutura de pacotes + H2 (Closes #1)

---
+++ backend/feira-tipo-api/src/main/java/br/unifor/feiratipoapi/FeiraTipoApiApplication.java
@@ -0,0 +1,13 @@
+package br.unifor.feiratipoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiraTipoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiraTipoApiApplication.class, args);
+	}
+
+}
